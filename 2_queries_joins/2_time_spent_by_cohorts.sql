9b5b248d7297bd64042a262cf0d9431329c71ea2
Caitie Garrood <caitlinanneg@gmail.com>
2021-03-01 16:51:51 -0500

    Updates second query

--- 2_queries_joins/2_time_spent_by_cohorts.sql
+++ 2_queries_joins/2_time_spent_by_cohorts.sql
@@ -2,4 +2,5 @@ SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = 'FEB12';
+WHERE cohorts.name = 'FEB12'
+AND end_date IS NOT NULL;
