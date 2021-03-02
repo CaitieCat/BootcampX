cdf0a4afb49a735ea4639b9df8bbeebab8902dd2
Caitie Garrood <caitlinanneg@gmail.com>
2021-03-02 13:08:16 -0500

    Adds two new tables and data to new tables

--- 3_queries_group_by/3_cohorts_biggerthan_18.sql
+++ 3_queries_group_by/3_cohorts_biggerthan_18.sql
@@ -1,5 +1,6 @@
 SELECT cohorts.name as cohort_name, count(*) as student_count
-FROM students JOIN cohorts.id = cohort_id
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
 GROUP BY cohort_name
 HAVING count(students.*) >= 18
 ORDER BY student_count;
